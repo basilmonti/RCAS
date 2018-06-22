cbf022995fd81e3d8837d61dcd10d91664a9c67c
lisab <basil.montemitro@ibv.ch>
2018-06-22 14:54:32 +0200

    StartWindowView

---
+++ src/rcas/controller/StartWindowViewController.java
@@ -0,0 +1,37 @@
+package rcas.controller;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.layout.GridPane;
+import rcas.model.MagicFormulaTireModel;
+import rcas.model.RaceCar;
+import rcas.model.TireModel;
+import rcas.util.CorneringAnalyserUtil;
+
+import java.util.Iterator;
+import java.util.ResourceBundle;
+
+public class StartWindowViewController {
+
+	@FXML
+	private GridPane mainPane;
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	public void initialize() {
+
+	}
+
+	public void handleNewCarBtn(ActionEvent actionEvent) {
+	}
+
+	public void handleOpenCarBtn(ActionEvent actionEvent) {
+	}
+
+	public void handleRecentCarBtn(ActionEvent actionEvent) {
+	}
+}
