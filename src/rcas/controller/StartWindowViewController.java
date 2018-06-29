cb7c11f30b71cc86c3715048b6b2af45bebc882e
lisab <basil.montemitro@ibv.ch>
2018-06-29 16:24:05 +0200

    StartWindow Funktionalität

--- src/rcas/controller/StartWindowViewController.java
+++ src/rcas/controller/StartWindowViewController.java
@@ -3,16 +3,22 @@ package rcas.controller;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import rcas.model.MagicFormulaTireModel;
 import rcas.model.RaceCar;
 import rcas.model.TireModel;
 import rcas.util.CorneringAnalyserUtil;
 
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class StartWindowViewController {
@@ -27,6 +33,7 @@ public class StartWindowViewController {
     private Button btnOpenCar;
     @FXML
     private Button btnRecentCar;
+    private Stage stage;
 
 	@FXML
 	public void initialize() {
@@ -35,6 +42,24 @@ public class StartWindowViewController {
 
 	public void handleNewCarBtn(ActionEvent actionEvent) {
 	    //TODO Connect to RC Main Menu with default Values
+		try
+		{
+			stage = (Stage) mainPane.getScene().getWindow();
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(StartWindowViewController.class.getResource("../../RCDetailView.fxml"));
+			ResourceBundle resourceBundle = ResourceBundle.getBundle("RCASResources");
+			fxmlLoader.setResources(resourceBundle);
+
+			Parent root = fxmlLoader.load();
+
+			stage.setScene(new Scene(root));
+			stage.show();
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public void handleOpenCarBtn(ActionEvent actionEvent) {
